f559118eaaf8db11c4baee51b135f9e9016629ec
Sana <guru-prasad.sana@capgemini.com>
2019-04-30 19:35:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'fun-video';
+ openNav():void {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
+
+
+openLogin() {
+  document.getElementById("login").onclick = function () {
+    location.href = "login.html";
+  };
+}
+
+openPremium() {
+  document.getElementById("premium").onclick = function () {
+    location.href = "premium.html";
+  };
+}
 }
---
+++ src/app/premium/premium.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+    declarations: [
+
+    ],
+
+    imports :[
+        BrowserModule
+    ],
+
+    exports:[
+        
+    ]
+})
+export class PremiumModule{
+
+
+}
